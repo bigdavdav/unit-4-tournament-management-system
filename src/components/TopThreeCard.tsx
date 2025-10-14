e35a6e05c5f394f9e4124e73621db6752324b20f
Davi Almeida 1 <davi.castelo.branco.almeida@gmail.com>
2025-10-14 16:43:31 +0100

    changed layout for tournament ranks

--- src/components/TopThreeCard.tsx
+++ src/components/TopThreeCard.tsx
@@ -2,17 +2,31 @@ import { TrophyIcon } from "@phosphor-icons/react"
 
 import styles from "./TopThreeCard.module.css"
 
-interface ColorNameProps {
+interface TopThreeCardProps {
   colorName: 'bronze' | 'silver' | 'gold'
 }
 
-export function TopThreeCard({ colorName }: ColorNameProps) {
+export function TopThreeCard({ colorName }: TopThreeCardProps) {
   return (
     <div className={` ${ styles.topThreeCard } ${ colorName == 'bronze' ? styles.bronze : '' } ${ colorName == 'silver' ? styles.silver : '' } ${ colorName == 'gold' ? styles.gold : '' } `}>
+
       <div className={ styles.teamName }>
         <TrophyIcon size={48} />
-        <h3>Team 1</h3>
+        <h2>Team 1</h2>
       </div>
+
+      <div className={ styles.teamStats }>
+        <span>
+          <b>Total Points: </b>
+          25
+        </span>
+
+        <span>
+          <b>Events Completed: </b>
+          2
+        </span>
+      </div>
+
     </div>
   )
 }
--- src/pages/TourneyRanks.tsx
+++ src/pages/TourneyRanks.tsx
@@ -1,14 +1,17 @@
-import { TrophyIcon } from "@phosphor-icons/react"
-
 import { TopThreeCard } from "../components/TopThreeCard"
 import styles from "./TourneyRanks.module.css"
 
 export function TourneyRanks() {
   return (
     <main>
-      <TopThreeCard colorName="bronze" />
-      <TopThreeCard colorName="silver" />
-      <TopThreeCard colorName="gold" />
+      <h1>Tournament Ranking</h1>
+      
+      <section className={ styles.topThreeContainer }>
+        <TopThreeCard colorName="silver" />
+        <TopThreeCard colorName="gold" />
+        <TopThreeCard colorName="bronze" />
+      </section>
+
     </main>
   )
 }
